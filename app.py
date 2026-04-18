bd53ce774e05927a7d9a71b2bc77cf2b07969a79
Bombonzinha <medifisq@gmail.com>
2026-04-18 17:02:29 -0300

    feat(1.1.2): Inicializar la estructura base del proyecto

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
+# AQUÍ IRÁN LAS PRUEBAS PRUEBAS UNITARAS Y EN EL FUTURO SERÁ EL FRONTEND
 import streamlit as st
 
-st.title('Mi App')
+st.title('Analizador de chats de Whatsapp')
 st.write('Hola mundo')
