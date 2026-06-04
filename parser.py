37312e2ce0a22bd7a90774fc5b3d643f6599a67f
Male <lesc.malena@gmail.com>
2026-06-04 17:41:35 -0300

    fix(1.2.4): Procesamiento del Archivo

--- parser.py
+++ parser.py
@@ -2,21 +2,58 @@
 import re
 import pandas as pd
 import emoji as emoji_lib
+import zipfile
+import io
 
 # Patrón que matchea el formato estándar de exportación de WhatsApp (Android)
 # Ejemplo de línea: 12/4/25, 21:03 - Nico: hola cómo andan
-PATTERN = re.compile(
+PATTERN_ANDROID = re.compile(
     r"(\d{1,2}/\d{1,2}/\d{2,4}),\s(\d{1,2}:\d{2})\s-\s([^:]+):\s(.*)"
+)
+# Patrón que matchea el formato estándar de exportación de WhatsApp (IOS)
+# Ejemplo de línea: [14/05/26 15:30:12] Nico: hola cómo andan
+PATTERN_IOS = re.compile(
+    r"\[(\d{1,2}/\d{1,2}/\d{2,4}),\s([^\]]+)\]\s([^:]+):\s(.*)"
 )
 
 def parse_chat(file):
-    """Lee el archivo subido y devuelve un DataFrame con columnas: fecha, usuario, mensaje."""
-    lineas = file.read().decode("utf-8").splitlines()
     registros = []
     mensaje_actual = None
 
+    file.seek(0)
+
+    extension = file.name.split(".")[-1].lower()
+
+    if extension == "txt":
+        """Lee el archivo subido y devuelve un DataFrame con columnas: fecha, usuario, mensaje."""
+        lineas = file.read().decode("utf-8").splitlines()
+
+    elif extension == "zip":
+        
+        with zipfile.ZipFile(io.BytesIO(file.read())) as zip_ref:
+
+            archivos_txt = [
+                nombre
+                for nombre in zip_ref.namelist()
+                if nombre.endswith("chat.txt")
+            ]
+
+            if not archivos_txt:
+                return pd.DataFrame()
+
+            with zip_ref.open(archivos_txt[0]) as txt:
+                
+                lineas = txt.read().decode("utf-8").splitlines()
+
+    else:
+        return pd.DataFrame()
+
     for linea in lineas:
-        match = PATTERN.match(linea)
+        match = PATTERN_ANDROID.match(linea)
+
+        if not match:
+            match = PATTERN_IOS.match(linea)
+
         if match:
             # Si había un mensaje acumulado, lo guardamos antes de arrancar uno nuevo
             if mensaje_actual:
